64d155fbfe9e6521d9e479b81ae1b5e09668a2ca
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2018-11-12 11:04:12 -0600

    removed default added my own p tag

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Button, Image, Label, Well, ListGroup } from 'react-bootstrap';
-import logo from "./hycabp.png";
 import { remote } from "electron";
 
 class App extends Component {
   render() {
-    return <Well>
-            <Image src={logo} alt="App Logo" height="64px"></Image>
-            <p>
-              React <sup><Label>Component</Label></sup>
-            </p>
-          </Well>;
-  }
+    return 
+      <div>
+      <p>This is a test</p>
+      </div>
+     }
 }
 
 ReactDOM.render(
